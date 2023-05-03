022ddc4439e4069529b1a8a5230b380783a25d6c
sk <sebastian.kuppa@web.de>
2023-05-03 13:46:21 +0800

    added http response basic sites, and database sqlite

--- base/views.py
+++ base/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('<h2>HOME</h2>')
+
+
+def post(request):
+    return HttpResponse('<h2>POST</h2>')
+
+
+def posts(request):
+    return HttpResponse('<h2>POSTS</h2>')
+
+
+def profile(request):
+    return HttpResponse('<h2>PROFILE</h2>')
